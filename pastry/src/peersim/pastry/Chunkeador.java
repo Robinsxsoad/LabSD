7f14724e51cc7a170239bcbd1dfdaa4d8f4de3af
Robinson Oyarzún <robinson.oyarzun@usach.cl>
2016-06-24 15:30:16 -0400

    Agregada función que al parecer guarda las partes en una lista, como string. Aún no probada.

--- pastry/src/peersim/pastry/Chunkeador.java
+++ pastry/src/peersim/pastry/Chunkeador.java
@@ -48,7 +48,49 @@ public class Chunkeador{
 //     return Arrays.asList(files);
 // }
     
+        public static List<Byte> cortarCancion(String nombre){
+            String fileName = "song.mp3";
 
+        try {
+            // Use this for reading the data.
+            byte[] buffer = new byte[128000];
+
+            FileInputStream inputStream = 
+                new FileInputStream(fileName);
+
+            // read fills buffer with data and returns
+            // the number of bytes read (which of course
+            // may be less than the buffer size, but
+            // it will never be more).
+            int total = 0;
+            int nRead = 0;
+            while((nRead = inputStream.read(buffer)) != -1) {
+                // Convert to String so we can display it.
+                // Of course you wouldn't want to do this with
+                // a 'real' binary file.
+                // System.out.println(new String(buffer));
+                System.out.println("Leí la primera partición de tamaño "+buffer.length+"\n");
+                total += nRead;
+            }   
+
+            // Always close files.
+            inputStream.close();        
+
+            System.out.println("Read " + total + " bytes");
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+            }
+        }
 
         public static void main(String [] args) {
 
@@ -93,8 +135,8 @@ public class Chunkeador{
                 + fileName + "'");                  
             // Or we could just do this: 
             // ex.printStackTrace();
+            }
         }
-    }
     
 
 } // End of class
