9ffa4750a0c74305fe834b8a9ac9e4062aa0584a
Robinson Oyarzún <robinson.oyarzun@usach.cl>
2016-06-27 18:55:38 -0400

    Corrección en vista no desplegada

--- App/src/app/App.java
+++ App/src/app/App.java
@@ -5,6 +5,8 @@
  */
 package app;
 
+import vistas.VistaCliente;
+
 /**
  *
  * @author robin
@@ -15,7 +17,9 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        VistaCliente vistaCliente; // Crea instancia de la vista de inicio del cliente
+        vistaCliente = new VistaCliente();
+        vistaCliente.setVisible(true); // Hace visible la ventana creada
     }
     
 }
